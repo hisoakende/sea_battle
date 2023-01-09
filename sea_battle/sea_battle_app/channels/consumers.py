657609b6fdb1b6d13db3f19e0e7fe0cab1ebc1cd
hisoakende <hisoakende@gmail.com>
2023-01-09 17:20:46 +0500

    added ability to set initial ships coordinates from client

--- sea_battle/sea_battle_app/channels/consumers.py
+++ sea_battle/sea_battle_app/channels/consumers.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any, Union, Optional
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
@@ -25,10 +25,10 @@ class BattleConsumer(JsonWebsocketConsumer):
         self.set_player()
 
         if self.battle_model.first_player and self.battle_model.second_player:
-            self.request_battle_info()
-            self.send_message_to_opponent({'type': 'info', 'message': 'opponent connected'})
+            self.send_message_to_opponent('give_battle_info', {})
+            self.send_message_to_opponent('send_json', {'type': 'info', 'body': 'opponent connected'})
         else:
-            self.battle_info = create_battle()
+            self.battle_fields = create_battle()
 
         self.accept()
 
@@ -42,42 +42,70 @@ class BattleConsumer(JsonWebsocketConsumer):
         if not self.battle_model.first_player and not self.battle_model.second_player:
             self.battle_model.delete()
         else:
-            self.send_message_to_opponent({'type': 'info', 'message': 'opponent disconnected'})
+            self.send_message_to_opponent('send_json', {'type': 'info', 'body': 'opponent disconnected'})
 
     def send_json(self, content: dict[str, Any], close: bool = False) -> None:
         super().send_json(content['content'])
 
+    def receive_json(self, content: Union[list, dict[str, Any]], **kwargs: Any) -> None:
+        match content:
+            case {'type': 'load ships coordinates', 'body': body}:
+                self.load_ships_coordinates(body)
+            case _:
+                self.process_invalid_request(content)
+
+    def load_ships_coordinates(self, ships_coordinates: list[list[list[int]]]) -> None:
+        condition, message = validate_ships_coords(ships_coordinates)
+        if condition:
+            self.battle_fields[self.player_number - 1].ships_coordinates.extend(ships_coordinates)
+            self.give_battle_info()
+            self.send_json({'content': {'type': 'success', 'body': 'ships successfully placed'}})
+        else:
+            self.send_json({'content': {'type': 'error', 'body': message}})
+
+    def process_invalid_request(self, body: Union[list, dict[str, Any]]) -> None:
+        match body:
+            case {'type': _, 'body': _}:
+                self.send_json({'content': {'type': 'error', 'body': 'unknown request type'}})
+            case {'body': _}:
+                self.send_json({'content': {'type': 'error', 'body': 'request must have \'type\' field'}})
+            case {'type': _}:
+                self.send_json({'content': {'type': 'error', 'body': 'request must have \'body\' field'}})
+            case _:
+                self.send_json({'content': {'type': 'error', 'body': 'request must have \'type\' and \'body\' fields'}})
+
     @property
-    def opponent(self) -> Player:
-        if self.battle_model.first_player == self.player or self.battle_model.first_player is None:
+    def opponent(self) -> Optional[Player]:
+        if self.player_number == 1:
             return self.battle_model.second_player
         return self.battle_model.first_player
 
-    def send_message_to_opponent(self, content: dict[str, Any]) -> None:
-        async_to_sync(self.channel_layer.send)(
-            self.opponent.channel_name, {'type': 'send_json', 'content': content}
-        )
+    def send_message_to_opponent(self, func_name: str, content: dict[str, Any]) -> None:
+        """
+        The function allows you to send messages both opponents consumer
+        and opponents client depending on which 'func_name' is received
+        """
+
+        if self.opponent is not None:
+            async_to_sync(self.channel_layer.send)(
+                self.opponent.channel_name, {'type': func_name, 'content': content}
+            )
 
     def set_player(self) -> None:
         self.player = Player.objects.create(channel_name=self.channel_name)
 
         if self.battle_model.first_player is None:
             self.battle_model.first_player = self.player
+            self.player_number = 1
         else:
             self.battle_model.second_player = self.player
+            self.player_number = 2
 
         self.battle_model.save()
 
-    def request_battle_info(self) -> None:
-        async_to_sync(self.channel_layer.send)(
-            self.opponent.channel_name, {'type': 'give_battle_info'}
-        )
-
-    def give_battle_info(self, content: dict[str, Any]) -> None:
+    def give_battle_info(self, *args: Any) -> None:
         self.battle_model.refresh_from_db()
-        async_to_sync(self.channel_layer.send)(
-            self.opponent.channel_name, {'type': 'set_battle_info', 'battle_info': self.battle_info.as_dict()}
-        )
+        self.send_message_to_opponent('set_battle_info', {'battle_info': self.battle_fields.as_dict()})
 
     def set_battle_info(self, content: dict[str, Any]) -> None:
-        self.battle_info = BattleInfo.from_dict(content['battle_info'])
+        self.battle_fields = BattleInfo.from_dict(content['content']['battle_info'])
--- sea_battle/sea_battle_app/models.py
+++ sea_battle/sea_battle_app/models.py
@@ -25,13 +25,23 @@ class BattleInfoManager(models.Manager):
 
 
 class Battle(models.Model):
-    """The sea battle model"""
+    """
+    The sea battle model
+
+    You can define stage of battle by 'who_win' and 'whose move':
+    1) if who_win is null and whose_move is null then battle is preparing (players set ships)
+    2) if who_win is null but whose_move is not null then game in progress
+    3) if who_win is not null and whose_move is null then game is finished
+    """
+
+    player_choices = (('first', 'first'), ('second', 'second'))
 
     address = models.CharField(max_length=127, unique=True)
     first_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='first')
     second_player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='second')
     state = models.JSONField(null=True)
-    who_win = models.CharField(max_length=15, choices=(('first', 'first'), ('second', 'second')), null=True)
+    who_win = models.CharField(max_length=15, choices=player_choices, null=True)
+    whose_move = models.CharField(max_length=15, choices=player_choices, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
     objects = BattleInfoManager()
